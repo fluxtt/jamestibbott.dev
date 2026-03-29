e7b721b0d647758da0a7c2b40b5a9fa7df413790
James Tibbott <100445187+fluxtt@users.noreply.github.com>
2026-03-29 16:47:40 +0100

    Refactor dashboard styles: remove unused CSS file and update stylesheet link in HTML

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>jamestibbott.dev</title>
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
-<body>
+<body class="home-body">
     <div class="hero">
         <h1>Welcome to jamestibbott.dev</h1>
         <p>There's not much here yet, but check back soon!</p>
