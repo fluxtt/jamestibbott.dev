2c18aa45dfe1e44f9289cfd87cd3df8ab79d4c8b
James Tibbott <100445187+fluxtt@users.noreply.github.com>
2026-06-03 20:41:48 +0100

    Include logo image in navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,5 +1,9 @@
 <nav class="navbar">
-    <div class="logo"><a href="/">JT</a></div>
+    <div class="logo">
+        <a href="/">
+            <img src="../assets/images/default-logo.png" alt="Logo" class="logo-image">
+        </a>
+    </div>
     <ul class="nav-links">
         <li><a href="/projects">Projects</a></li>
         <li><a href="/utilities">Utilities</a></li>
